c6dfdcccce18d1f4c0d5cf1751eddc4c603a86a7
Aniket <Aniket@WIN-PC>
2018-01-19 23:56:54 -0800

    Commoted By Aniket

---
+++ HB-With-log4j/src/main/java/com/cb/main/log4jExample.java
@@ -0,0 +1,29 @@
+package com.cb.main;
+
+import java.io.IOException;
+
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.MDC;
+import org.apache.log4j.PropertyConfigurator;
+
+public class log4jExample {
+	/* Get actual class name to be printed on */
+	static Logger log = Logger.getLogger(log4jExample.class.getName());
+	static int user = 1;
+
+	public static void main(String[] args) throws IOException, SQLException {
+		MDC.put("user", 101);
+		 //PropertyConfigurator.configure("src/main/java/log4j.properties");
+		log.info(user);
+		log.debug("in Main");
+		String s = "";
+		for (int i = 1; i <= 100; i++) {
+			s += i + " ";
+		}
+		log.info(s);
+		log.info("main end");
+		System.out.println("end");
+	}
+}
